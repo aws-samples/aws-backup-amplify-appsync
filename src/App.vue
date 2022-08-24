c064eae59498f055e50ce60cd3d247732e8f9d29
Kevin Schwarz <kschwa@amazon.com>
2022-08-24 11:59:05 -0400

    schema changes to js

--- src/App.vue
+++ src/App.vue
@@ -19,31 +19,35 @@ import "@aws-amplify/ui-vue/styles.css";
         <div class="row">
           <div class="col">
             <div class="row">
-              <h3>Create a blog category</h3>
-              <label for="firstName">Enter Blog Category</label>
+              <h3>Create a Category</h3>
+              <label for="name">Enter Category</label>
             </div>
             <div class="row">
               <input
                 type="text"
-                v-model="blog.name"
-                placeholder="Blog Name"
+                v-model="category.name"
+                placeholder="Category Name"
                 id="name"
               />
             </div>
             <div class="row">
               <input
                 type="button"
-                value="Create Blog"
-                @click="createNewBlog(this.blog)"
+                value="Create Category"
+                @click="createNewCategory(this.category)"
               />
             </div>
           </div>
           <div class="col">
-            <div class="row" v-for="blog in this.blogs" v-bind:key="blog.id">
+            <div
+              class="row"
+              v-for="category in this.categories"
+              v-bind:key="category.id"
+            >
               <div class="row">
-                <h3>{{ blog.name }}</h3>
-                <ul v-for="post in blog.posts.items" v-bind:key="post.id">
-                  <li>{{ post.title }}</li>
+                <h3>{{ category.name }}</h3>
+                <ul v-for="item in category.items.items" v-bind:key="item.id">
+                  <li>{{ item.title }}</li>
                 </ul>
               </div>
             </div>
@@ -56,33 +60,33 @@ import "@aws-amplify/ui-vue/styles.css";
 
 <script>
 import { API } from "aws-amplify";
-import { createBlog } from "./graphql/mutations";
-import { listBlogs } from "./graphql/queries";
+import { createCategory } from "./graphql/mutations";
+import { listCategories } from "./graphql/queries";
 
 export default {
   name: "AppVue",
   data() {
     return {
-      blog: {},
-      post: {},
-      blogs: [],
+      category: {},
+      item: {},
+      categories: [],
     };
   },
   async beforeMount() {
-    this.loadBlogs();
+    this.loadCategories();
   },
   methods: {
-    async loadBlogs() {
-      const blogs = await API.graphql({ query: listBlogs });
-      console.log(blogs);
-      this.blogs = blogs.data.listBlogs.items;
+    async loadCategories() {
+      const categories = await API.graphql({ query: listCategories });
+      console.log(categories);
+      this.categories = categories.data.listCategories.items;
     },
-    async createNewBlog(blog) {
+    async createNewCategory(category) {
       await API.graphql({
-        query: createBlog,
-        variables: { input: blog },
+        query: createCategory,
+        variables: { input: category },
       });
-      this.loadBlogs(); // shortcut, should use subscriptions
+      this.loadCategories(); // shortcut, should use subscriptions
     },
   },
 };
